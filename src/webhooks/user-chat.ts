0beb0ab7a6061ed3b28778f1fbff3295f2c82af6
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-04-29 10:45:55 -0400

    complete

--- src/webhooks/user-chat.ts
+++ src/webhooks/user-chat.ts
@@ -10,38 +10,42 @@ import { v4 } from "uuid";
 export const userChat = async (req: Request, res: Response) => {
   // get the messages from the channel
   try {
-    const { channel_id } = req.body;
-    const channel = new ChatChannel(channel_id);
-    const messages = await channel.getChannelMessages();
+    const { channel_id, user } = req.body;
+    if (user.id !== "assistant") {
+      const channel = new ChatChannel(channel_id);
+      const messages = await channel.getChannelMessages();
 
-    // Transform messages into something that can be sent
-    // to chatGpt
-    const messagesChatGptFormat: MessageChatGPTType[] = messages.map(
-      (message) => {
-        return {
-          role:
-            message.user && message.user.id === "assistant"
-              ? "assistant"
-              : "user",
-          content: message.text,
-        };
-      }
-    );
-    const chatGPTPayload: PostChatGPTDataType = {
-      model: "gpt-3.5-turbo",
-      messages: [...messagesChatGptFormat],
-    };
-    const chatGPTResponse = await postChatGpt(chatGPTPayload);
+      // Transform messages into something that can be sent
+      // to chatGpt
+      const messagesChatGptFormat: MessageChatGPTType[] = messages.map(
+        (message) => {
+          return {
+            role:
+              message.user && message.user.id === "assistant"
+                ? "assistant"
+                : "user",
+            content: message.text,
+          };
+        }
+      );
+      const chatGPTPayload: PostChatGPTDataType = {
+        model: "gpt-3.5-turbo",
+        messages: [...messagesChatGptFormat],
+      };
+      const chatGPTResponse = await postChatGpt(chatGPTPayload);
 
-    await channel.sendMessage(
-      new ChatMessage(
-        v4(),
-        chatGPTResponse.content,
-        new ChatUser(chatGPTResponse.role)
-      )
-    );
+      await channel.sendMessage(
+        new ChatMessage(
+          v4(),
+          chatGPTResponse.content,
+          new ChatUser(chatGPTResponse.role)
+        )
+      );
 
-    res.status(200).json(chatGPTResponse);
+      res.status(200).json(chatGPTResponse);
+    } else {
+      res.status(200);
+    }
   } catch (e: any) {
     console.log(e);
     res.status(500).send(e.message);
