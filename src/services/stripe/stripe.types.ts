b742a42f3e2b5e214e2c948d8111fec42d8c3cf9
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-05-02 07:59:14 -0400

    added StripeCutomer class

--- src/services/stripe/stripe.types.ts
+++ src/services/stripe/stripe.types.ts
@@ -1,4 +1,4 @@
-export type StripeCustomer = {
+export class StripeCustomer {
   id: string;
   object: "customer";
   address: {
@@ -15,4 +15,25 @@ export type StripeCustomer = {
   name: string | null;
   metadata: { [key: string]: string };
   phone: string | null;
-};
+
+  constructor(customerData: Partial<StripeCustomer>) {
+    this.id = customerData.id || "";
+    this.object = "customer";
+    this.address = customerData.address || null;
+    this.created = customerData.created || 0;
+    this.email = customerData.email || null;
+    this.description = customerData.description || null;
+    this.name = customerData.name || null;
+    this.metadata = customerData.metadata || {};
+    this.phone = customerData.phone || null;
+  }
+
+  toJson(): string {
+    return JSON.stringify(this);
+  }
+
+  static fromJson(json: string): StripeCustomer {
+    const parsedData = JSON.parse(json);
+    return new StripeCustomer(parsedData);
+  }
+}
