40a0193c8c002edd4b5f5473f5b9dbdc7adf91cb
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-05-04 17:57:17 -0400

    got distracted

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { createUser, getUser } from "@/controllers/user.controllers";
 
 const router = express.Router();
+
 // user
 router.post("/create", createUser);
 router.get("/get/:authUserId", getUser);
+// end user routes
 
 export default router;
